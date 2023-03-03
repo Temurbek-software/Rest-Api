019d5393304a98d81826d508e34d0b84dfce120a
Temurbek Zarifov <67744075+Temurbek-software@users.noreply.github.com>
2023-03-03 09:30:10 +0500

    created multi module project parts

---
+++ Rest-Api.Model/Entities/Books.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rest_Api.Model.Entities
+{
+    public class Books
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }   
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public double Prices { get; set; }
+        public int PageNumbers { get; set; }  
+    }
+}
